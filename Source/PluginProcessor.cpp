31dbc53330f0ae3ddf20f16234386e42a23145d6
Oli Partridge <op642@york.ac.uk>
2025-03-31 02:33:19 +0100

    Randdom EQ Parameters on GUI

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -95,6 +95,17 @@ void OpenAIR_ConvolverAudioProcessor::prepareToPlay (double sampleRate, int samp
 {
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
+    
+    juce::dsp::ProcessSpec spec;
+    
+    spec.maximumBlockSize = samplesPerBlock;
+    
+    spec.numChannels = 1;
+    
+    spec.sampleRate = sampleRate;
+    
+    leftChain.prepare(spec);
+    rightChain.prepare(spec);
 }
 
 void OpenAIR_ConvolverAudioProcessor::releaseResources()
@@ -144,18 +155,15 @@ void OpenAIR_ConvolverAudioProcessor::processBlock (juce::AudioBuffer<float>& bu
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    juce::dsp::AudioBlock<float> block(buffer);
+    auto leftblock = block.getSingleChannelBlock(0);
+    auto rightblock = block.getSingleChannelBlock(1);
+    
+    juce::dsp::ProcessContextReplacing<float> leftContext(leftblock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(rightblock);
+    
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
 }
 
 //==============================================================================
@@ -166,7 +174,8 @@ bool OpenAIR_ConvolverAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* OpenAIR_ConvolverAudioProcessor::createEditor()
 {
-    return new OpenAIR_ConvolverAudioProcessorEditor (*this);
+//    return new OpenAIR_ConvolverAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -175,6 +184,44 @@ void OpenAIR_ConvolverAudioProcessor::getStateInformation (juce::MemoryBlock& de
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout
+    OpenAIR_ConvolverAudioProcessor::createParameterLayout()
+{
+        juce::AudioProcessorValueTreeState::ParameterLayout layout;
+        layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("LowCut Freq",1),
+                                                               "LowCut Freq",
+                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+                                                               20.f));
+        layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("HighCut Freq",1),
+                                                               "HighCut Freq",
+                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+                                                               20000.f));
+        layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("Peak Freq",1),
+                                                               "Peak Freq",
+                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+                                                               750.f));
+        layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("Peak Gain",1),
+                                                               "Peak Gain",
+                                                               juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f),
+                                                               0.0f));
+        layout.add(std::make_unique<juce::AudioParameterFloat>(juce::ParameterID("Peak Quality",1),
+                                                               "Peak Quality",
+                                                               juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 1.f),
+                                                               1.f));
+        
+        juce::StringArray stringArray;
+        for(int i = 0; i < 4; ++i)
+        {
+            juce::String str;
+            str << (12 + i*12);
+            str << "dB/Oct";
+            stringArray.add(str);
+        }
+        layout.add(std::make_unique<juce::AudioParameterChoice>(juce::ParameterID("LowCut Slope",1), "LowCut Slope", stringArray, 0));
+        layout.add(std::make_unique<juce::AudioParameterChoice>(juce::ParameterID("HighCut Slope",1), "HighCut Slope", stringArray, 0));
+        return layout;
 }
 
 void OpenAIR_ConvolverAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
