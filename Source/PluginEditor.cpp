94b210adb16fed807581709144ba766e9aa0adef
Oli Partridge <op642@york.ac.uk>
2025-04-23 01:22:07 +0100

    Updated GUI with Logo

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -20,13 +20,15 @@ OpenAIRConvolverAudioProcessorEditor::~OpenAIRConvolverAudioProcessorEditor()
 //==============================================================================
 void OpenAIRConvolverAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // Find a way to load file
-    //g.drawImage(juce::ImageCache::getFromFile(juce::File("Assets/Image/OpenAir_logo.png")), getLocalBounds().toFloat());
     g.fillAll(juce::Colours::black);
+    // Find a way to load file
+    background = juce::ImageCache::getFromMemory(BinaryData::OpenAir_logo_png, BinaryData::OpenAir_logo_pngSize);
+    g.drawImageWithin(background, 15, 30, 0.95*getWidth(), 0.2*getHeight(), juce::RectanglePlacement::fillDestination, false);
+    
 
     g.setColour (juce::Colours::white);
     g.setFont (juce::FontOptions (30.0f));
-    g.drawFittedText ("OpenAIR Convolver", getLocalBounds(), juce::Justification::centred, 1);
+    //g.drawFittedText ("OpenAIR Convolver", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 OpenAIRConvolverAudioProcessorEditor::OpenAIRConvolverAudioProcessorEditor (OpenAIRConvolverAudioProcessor& p)
@@ -35,9 +37,9 @@ OpenAIRConvolverAudioProcessorEditor::OpenAIRConvolverAudioProcessorEditor (Open
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     addAndMakeVisible(loadIRButton);
-    loadIRButton.setButtonText("Load IR");
+    loadIRButton.setButtonText("Load B-Format IR");
     loadIRButton.onClick = [this] {
-        fileChooser = std::make_unique<juce::FileChooser>("Select an IR file", audioProcessor.getRoot(), "*");
+        fileChooser = std::make_unique<juce::FileChooser>("Select a B-Format IR file", audioProcessor.getRoot(), "*");
 
         const auto fileChooserFlags = juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles;
 
@@ -47,7 +49,6 @@ OpenAIRConvolverAudioProcessorEditor::OpenAIRConvolverAudioProcessorEditor (Open
             if (file.existsAsFile())
             {
                 audioProcessor.loadIRFile(file);
-//                audioProcessor.preloadIRFile(file); // Preload the IR file
             }
         });
     };
@@ -63,9 +64,9 @@ void OpenAIRConvolverAudioProcessorEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
     const auto btnX = getWidth() * (0.21);
-    const auto btnY = getHeight() * (0.6);
+    const auto btnY = getHeight() * (0.45);
     const auto btnWidth = getWidth() * (0.59);
-    const auto btnHeight = getHeight() * (0.1); //JUCE_LIVE_CONSTANT
+    const auto btnHeight = (0.1) * getHeight(); //JUCE_LIVE_CONSTANT
     
     loadIRButton.setBounds(btnX, btnY, btnWidth, btnHeight);
 }
